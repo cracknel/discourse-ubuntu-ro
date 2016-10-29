95f02e36c022fddea756d71687f00b7a4b93e6c3
Alexandru Cucu <cracknel.org@gmail.com>
2016-10-29 14:44:25 +0300

    fix variables path

--- plugin.rb
+++ plugin.rb
@@ -6,7 +6,7 @@
 register_custom_html extraNavItem: "<li id='archive-menu-item'><a href='http://forum.ubuntu.ro'>Arhivă (forumul vechi)</a></li>"
 
 ## Override variables (used in other things)
-register_asset "stylesheet/common/variables.scss", :variables
+register_asset "common/variables.scss", :variables
 
 ## Global settings
 register_asset "stylesheet/global.scss"
